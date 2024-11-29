b6a0c385bed7fedaab4cf8e029db9d8b636b97a3
islam-foda <Eslam.foda@hotmail.com>
2024-11-30 00:17:36 +0200

    scnd try to fix image path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import { data } from "./data";
+import  data  from "./data";
 import Header from "./Header";
 import SectionFour from "./Section-four";
 import SectionOne from "./Section-one";
