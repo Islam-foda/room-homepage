24f5d332311c94071f285baa2e70e4d56e5a2c90
islam-foda <Eslam.foda@hotmail.com>
2024-11-29 23:43:09 +0200

    finish the page v1.0

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,20 @@
 export default function Header({ image, title }) {
+  const btnMenu = document.getElementById("imgBurger");
+  const imgClose = document.querySelector("[data-img-close]");
+  const navMenu = document.getElementsByClassName("nav-contain")[0];
+
+  if (btnMenu) {
+    btnMenu.addEventListener("click", () => {
+      imgClose.classList.add("close");
+      navMenu.classList.add("burger-menu");
+    });
+  }
+  if (imgClose) {
+    imgClose.addEventListener("click", () => {
+      navMenu.classList.remove("burger-menu");
+      imgClose.classList.remove("close");
+    });
+  }
   return (
     <header className="header">
       <nav>
@@ -7,22 +23,38 @@ export default function Header({ image, title }) {
             <img src="/src/assets/logo.svg" alt="room-logo" />
           </a>
         </div>
-        <ul className="nav-menu">
-          <li>
-            <a href="#">home</a>
-          </li>
-          <li>
-            <a href="#">shop</a>
-          </li>
-          <li>
-            <a href="#">about</a>
-          </li>
-          <li>
-            <a href="#">contact</a>
-          </li>
-        </ul>
-        <div className="nav-burger">
-          <img src="/src/assets/icon-hamburger.svg" alt="burger-icon" />
+        <button className="nav-burger" id="nav-burger">
+          <img
+            id="imgBurger"
+            data-img-burger="burger"
+            src="/src/assets/icon-hamburger.svg"
+            alt="burger-icon"
+          />
+          <img
+            data-img-close="close"
+            src="/src/assets/icon-close.svg"
+            alt="close-icon"
+            style={{ display: "none" }}
+          />
+        </button>
+
+        {/* add burger menu n the screen width 650px  */}
+        <div className="nav-contain ">
+          {" "}
+          <ul className="nav-menu">
+            <li>
+              <a href="#">home</a>
+            </li>
+            <li>
+              <a href="#">shop</a>
+            </li>
+            <li>
+              <a href="#">about</a>
+            </li>
+            <li>
+              <a href="#">contact</a>
+            </li>
+          </ul>
         </div>
       </nav>
       <div className="img-slider" id="slider">
